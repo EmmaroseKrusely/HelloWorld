4cac8656237ab0c53e0733c01b6de1354804e356
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2021-08-31 09:43:38 -0400

    Add files via upload

---
+++ Dancing/Dancing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dancing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("hello my name is ghost");
+            Console.ReadKey();
+
+        }
+    }
+}
