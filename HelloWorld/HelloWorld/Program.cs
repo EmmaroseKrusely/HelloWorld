04e5ade43b26a451a509add38e7f95072275dfcd
EmmaroseKrusely <70153909+EmmaroseKrusely@users.noreply.github.com>
2021-08-31 09:30:53 -0400

    Add files via upload

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World :)");
+
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"you are {input} years old, thats old");
+            Console.ReadKey();
+        }
+    }
+}
